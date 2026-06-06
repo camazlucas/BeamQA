b83884d5dee04b7adfff5f9e92fa7e9772e93134
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-05 22:25:22 -0300

    Recriacao dos Emeddings

--- Data/Graph_data/MetaQA/MetaQAProcess/embeddingtrain.py
+++ Data/Graph_data/MetaQA/MetaQAProcess/embeddingtrain.py
@@ -9,7 +9,7 @@ print("Entidades:", tf.num_entities)
 print("Relações:", tf.num_relations)
 
 training, testing, validation = tf.split(
-    ratios=(0.8, 0.1, 0.1),
+    ratios=(0.7, 0.15, 0.15),
     random_state=42
 )
 
@@ -27,6 +27,43 @@ result = pipeline(
     }
 )
 
-result.save_to_directory("complex_metaqa_100_inv")
+result.save_to_directory("new_complex_metaqa_100_inv")
 
 print("Treinamento concluído.")
+
+# -------------------------
+# Construção do nx_graph
+# -------------------------
+
+G = nx.DiGraph()
+
+id_to_entity = {
+    v: k
+    for k, v in tf.entity_to_id.items()
+}
+
+id_to_relation = {
+    v: k
+    for k, v in tf.relation_to_id.items()
+}
+
+for h, r, t in tf.mapped_triples.tolist():
+
+    head = id_to_entity[h]
+    relation = id_to_relation[r]
+    tail = id_to_entity[t]
+
+    G.add_edge(
+        head,
+        tail,
+        data=relation
+    )
+
+with open(
+    "complex_metaqa_100_inv/MetaQA_graph.pkl",
+    "wb"
+) as f:
+
+    pickle.dump(G, f)
+
+print("Grafo salvo.")
