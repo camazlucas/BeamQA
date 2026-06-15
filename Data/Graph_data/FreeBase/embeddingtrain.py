05eabeb5325cf203c4bb54e70b639964625b8a64
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-15 09:21:08 -0300

    Ajuste do script para conversão do KG em embeddings

--- Data/Graph_data/FreeBase/embeddingtrain.py
+++ Data/Graph_data/FreeBase/embeddingtrain.py
@@ -1,8 +1,21 @@
+import argparse
+
 from pykeen.triples import TriplesFactory
 from pykeen.pipeline import pipeline
 
-tf = TriplesFactory.from_path("kb.tsv",
-                              create_inverse_triples=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("--kb", type=str, required=True,
+                    help="Caminho para o arquivo TSV da base de conhecimento")
+
+parser.add_argument("--output", type=str, required=True,
+                    help="Diretório onde o kg será salvo")
+
+args = parser.parse_args()
+
+tf = TriplesFactory.from_path(
+    args.kb,
+    create_inverse_triples=True
+)
 
 print("Triplas:", tf.num_triples)
 print("Entidades:", tf.num_entities)
@@ -27,6 +40,6 @@ result = pipeline(
     }
 )
 
-result.save_to_directory("complex_metaqa_100_inv")
+result.save_to_directory(args.output)
 
 print("Treinamento concluído.")
