1d8db9d018ac44accf9dc4d16ab35eccc1ce0c73
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-15 04:09:08 -0300

    test do BART para WQSP

---
+++ WQSP/Modulo 1/test-BART.py
@@ -0,0 +1,34 @@
+import torch
+
+from transformers import (
+    BartTokenizer,
+    BartForConditionalGeneration
+)
+
+MODEL_DIR = "./Modelo1-BART"
+
+tokenizer = BartTokenizer.from_pretrained(MODEL_DIR)
+
+model = BartForConditionalGeneration.from_pretrained(MODEL_DIR)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model.to(device)
+model.eval()
+
+def predict(question):
+    inputs = tokenizer(question, return_tensors="pt").to(model.device)
+    outputs = model.generate(**inputs, max_length=128)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+while True:
+
+    question = input("\nPergunta: ")
+
+    if question.lower() == "exit":
+        break
+
+    print(predict(question))
+
+
+# print(predict("who directed movies written by Quentin Tarantino"))
