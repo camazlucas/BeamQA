6bd341f7231a94b22b190eed90f63e75f4f075f8
Lucas Camaz Ferreira <camazlucas@gmail.com>
2026-06-16 23:52:54 -0300

    remocao de debug

--- AtualizacaoMetaQA/evaluate.py
+++ AtualizacaoMetaQA/evaluate.py
@@ -238,16 +238,6 @@ with open(
                     path_model
                 )
 
-                ################################## DEBUG ###################
-                if total_questions < 5:
-                    print("\nPergunta:")
-                    print(question)
-
-                    print("\nPaths:")
-                    for p, s in zip(sample["paths"][:10], sample["scores"][:10]):
-                        print(f"{s:.4f} | {p}")
-                ##############################################################
-
             candidates = path_finder_candidates(
                 head,
                 sample["paths"],
